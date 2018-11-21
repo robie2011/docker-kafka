9cd9708ec599d6f19c1c60e0d6d69a5ce7e13714
Robert Rajakone <robert.rajakone@fhnw.ch>
2018-11-21 08:40:50 +0100

    delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /opt/kafka
-bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
-bin/kafka-server-start.sh -daemon config/server.properties
-
-# didn't work because of unsupported filesystem type issue
-# but is required to prevend exit docker container
-tail -f /tmp/kafka-logs/*.log
